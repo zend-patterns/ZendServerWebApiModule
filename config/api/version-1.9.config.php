2f9ce3b56151117846e30435933f9dafa10359f7
Slavey Karadzhov <slavey.karadzhov@zend.com>
2014-11-24 11:45:37 +0100

    Added new urlinsight methods request from R&D.

--- config/api/version-1.9.config.php
+++ config/api/version-1.9.config.php
@@ -80,7 +80,64 @@ If not supplied, default is 1.'),
                     )
                 ),
                 
+                'urlinsightGetUrls' => array(
+                    'options' => array(
+                        'route' => 'urlinsightGetUrls [--limit=] [--offset=] [--applicationId=] [--filter=] [--period=]',
+                        'defaults' => array(
+                            'controller' => 'webapi-api-controller',
+                            'action' => 'urlinsightGetUrls',
+                            'apiMethod' => 'get'
+                        ),
+                        'group' => 'urlinsight',
+                        'info' => array(
+                            'Get the list of url statistics.',
+                            array('--limit', 'The number of rows to retrieve. Default lists all rules up to an arbitrary limit set by the system.'),
+                            array('--offset', 'A paging offset to begin the rules list from. Default is 0.'),
+                            array('--applicationId', 'Display URLs of a specific application. If not supplied, display URLs from all the applications.'),
+                            array('--filter', 'Predefined filters/order
+1 - "most time consuming" - order by num of samples multiply average time, descending
+2 - "slowest response time" - order by average time descending
+3 - "most visited" - ordered by number of samples descending
+
+If not supplied, default is 1.'),
+                            array('--period', 'Period in hours. (one week is 24*7, etc…). Default is 24.'),
+                        )
+                    )
+                ),
                 
+                'urlinsightGetUrlInfo' => array(
+                    'options' => array(
+                        'route' => 'urlinsightGetUrlInfo --id= [--order=] [--period=]',
+                        'defaults' => array(
+                            'controller' => 'webapi-api-controller',
+                            'action' => 'urlinsightGetUrlInfo',
+                            'apiMethod' => 'get'
+                        ),
+                        'group' => 'urlinsight',
+                        'info' => array(
+                            'Get a single request statistics info with its requests. The requests can be filtered and ordered using `period` and `order` parameter.',
+                            array('--id', 'The url id to retrieve the info from.'),
+                            array('--order', 'The order of the requests list. The format is like in "order" clause in sql, e.g. "from_time desc" or "until_time".'),
+                            array('--period', 'Number of hours. Limit the requests list to a specific period - "period"| hours back until now. Default is 24 (i.e. by default bring requests from the last 24 hours)'),
+                        )
+                    )
+                ),
+                
+                'urlinsightGetZraySnapshots' => array(
+                    'options' => array(
+                        'route' => 'urlinsightGetZraySnapshots --resource_id=',
+                        'defaults' => array(
+                            'controller' => 'webapi-api-controller',
+                            'action' => 'urlinsightGetZraySnapshots',
+                            'apiMethod' => 'get'
+                        ),
+                        'group' => 'urlinsight',
+                        'info' => array(
+                            'Get stored z-ray snapshots for a requested resource ID.',
+                            array('--resource_id', 'The url id to retrieve the info from.'),
+                        )
+                    )
+                ),
             )
         )
     )
